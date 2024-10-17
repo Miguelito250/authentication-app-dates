d3c4e4408f568121faadc13896d9a75ee0a4cb22
Miguelito250 <mjackmeca@gmail.com>
2024-10-16 20:13:50 -0500

    feat: Add Unique Constraint To Username

--- Data/AuthenticationContext.cs
+++ Data/AuthenticationContext.cs
@@ -1,7 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using AuthenticationService.Models;
 
-public class AuthenticationContext : DbContext{
+public class AuthenticationContext : DbContext
+{
     public AuthenticationContext(DbContextOptions<AuthenticationContext> options)
         : base(options)
     {
@@ -9,4 +10,12 @@ public class AuthenticationContext : DbContext{
 
     public DbSet<User> Users => Set<User>();
     public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>()
+        .HasIndex(u => u.Username)
+        .IsUnique();
+    }
 }
